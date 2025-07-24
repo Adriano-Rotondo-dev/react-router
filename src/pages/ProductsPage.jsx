d05a4df8a8617e559b702f1db4d92032ac295e0c
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-07-24 19:52:32 +0200

    add axios library for AJAX request to get products from endpoint, add mapping with generated cards for product details, add css style for NavLink highlight

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,3 +1,47 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function ProductsPage() {
-  return <main></main>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => setProducts(res.data))
+      .catch((err) => console.error(err));
+  });
+
+  return (
+    <main>
+      <div className="container py-5">
+        <h2 className="mb-4">I nostri prodotti</h2>
+        <div className="row">
+          {products.map((product) => (
+            <div className="col-md-3 mb-4" key={product.id}>
+              <div className="card h-100">
+                <img
+                  src={product.image}
+                  className="card-img-top h-30 W-20 p-3"
+                  alt={product.title}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{product.title}</h5>
+                  <p className="card-text">{product.description}</p>
+                  <p className="card-text">{product.category}</p>
+                  <p className="card-text">
+                    Valutazione degli utenti: {product.rating.rate}
+                  </p>
+                  <p className="card-text">
+                    Numero di voti:
+                    {product.rating.count}
+                  </p>
+                  <p className="fw-bold text-primary">{product.price} €</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 }
