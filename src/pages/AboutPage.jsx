44ab9b8d6269db121ab90d2f1311255433ca05e7
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-07-24 19:23:42 +0200

    Add AboutUs page content with img and text description

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,3 +1,34 @@
 export default function AboutPage() {
-  return <main>About</main>;
+  return (
+    <main>
+      <section className="py-5 bg-light">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-md-6 mb-4 mb-md-0">
+              <img
+                src="img/contactUs.png"
+                alt="about_us"
+                className="img-fluid rounded shadow"
+              />
+            </div>
+            <div className="col-md-6">
+              <h2 className="display-5 fw-bold">Who's Foxy?🦊🛍️</h2>
+              <p className="lead">
+                Chi è Foxy? Foxy è una volpe curiosa, intraprendente e con un
+                debole per le cose belle (e strane). Ha trasformato la sua tana
+                in un piccolo grande bazar online: un posto dove puoi trovare di
+                tutto — oggetti utili, idee regalo originali, cose che non
+                sapevi ti servissero... ma che improvvisamente non puoi più
+                ignorare. FoxyShop non è un colosso globale, è un negozio con
+                anima: amatoriale, indipendente, ma organizzato con cura e
+                passione. Qui ogni prodotto è scelto con occhio attento e
+                spirito libero. Un po’ come Foxy: veloce, sveglia, imprevedibile
+                — ma sempre dalla tua parte.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
